0f667846b25fc216599d1ce5c39787c0cde4bf7b
zhengzhiyuan <zzyjiaxinmomo@163.com>
2021-01-28 11:26:34 +0800

    Add publictPath

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:8081/"
+        : "https://prod-test-header-nine.vercel.app/",
   },
 
   resolve: {
@@ -12,7 +15,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 8080,
+    port: 8081,
   },
 
   module: {
@@ -40,9 +43,12 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: "starter",
+      name: "consumer",
       filename: "remoteEntry.js",
-      remotes: {},
+      remotes: {
+        header:
+          "header@https://prod-test-header-nine.vercel.app/remoteEntry.js",
+      },
       exposes: {},
       shared: {
         ...deps,
@@ -60,4 +66,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
